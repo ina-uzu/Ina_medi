914d903f7a085dc2b8cd3df0033df31f1518bd03
= <=>
2018-08-05 13:52:35 +0900

    add Bnt event

--- app/src/main/java/com/example/ina97/myapplication/StoolActivity.java
+++ app/src/main/java/com/example/ina97/myapplication/StoolActivity.java
@@ -5,16 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StoolActivity extends Activity {
 
     Button bt_prev, bt_next;
+    TextView tv_stool_num;
+    String stool_num="1";
     public void onCreate(Bundle SavedInstanceState){
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_stool);
 
         bt_next=findViewById(R.id.Bnt_next);
         bt_prev=findViewById(R.id.Bnt_prev);
+        tv_stool_num = findViewById(R.id.tv_stool_num);
+
+        //stool num 얻어오기
+
+        //next bnt 텍스트 수전
+        bt_next.setText("등록");
+        tv_stool_num.setText("등록시, 오늘 총 "+stool_num +"로 기록됩니다");
 
         bt_prev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +40,7 @@ public class StoolActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Intent intent2 = new Intent(StoolActivity.this,MenuActivity.class);
+                Toast.makeText(getApplicationContext(), "성공적으로 등록되었습니다",Toast.LENGTH_LONG).show();
                 startActivity(intent2);
 
             }
--- app/src/main/java/com/example/ina97/myapplication/TimeDateActivity.java
+++ app/src/main/java/com/example/ina97/myapplication/TimeDateActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TimeDateActivity extends Activity {
     Button bt_prev, bt_next;
@@ -14,6 +15,7 @@ public class TimeDateActivity extends Activity {
         super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_tima_date);
 
+        //title 얻어오기
         Intent intent = getIntent();
         String page_title= intent.getStringExtra("Page");
 
@@ -35,8 +37,9 @@ public class TimeDateActivity extends Activity {
         bt_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(TimeDateActivity.this,StoolActivity.class);
-                startActivity(intent2);
+                Intent intent2 = new Intent(TimeDateActivity.this, StoolActivity.class);
+                Toast.makeText(getApplicationContext(),"Hello", Toast.LENGTH_LONG).show();
+                //startActivity(intent2);
 
             }
         });
